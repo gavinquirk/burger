406538f7ddbb4a71f14e80b4884decfeec520504
Gavin Quirk <gavinquirk@gmail.com>
2018-04-28 18:26:42 -0700

    initial commit with basic fs

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+// Inside the burgers_controller.js file, import the following:
+
+// Express
+var express = require('express')
+// burger.js
+var burger = require('')
+
+
+// Create the router for the app, and export the router at the end of your file.
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double Western', true), ('Big Mac', false), ('Cheeseburger', true);
